0782bb9722408fa217ed15f3cae2ac70a76deb52
GKentaurus <gkent@outlook.com>
2021-02-03 10:39:39 -0500

    Intento de deploy

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Pagina1Component } from './components/pagina1/pagina1.component';
+import { Pagina2Component } from './components/pagina2/pagina2.component';
+import { Pagina3Component } from './components/pagina3/pagina3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Pagina1Component,
+    Pagina2Component,
+    Pagina3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
